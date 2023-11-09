72a0b1b429844b3e108aae3f13fbd99662512346
gabriellaaaaaaa <132076667+gabriellaaaaaaa@users.noreply.github.com>
2023-11-09 11:33:31 -0300

    .1

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let contador=0
 while (contador<listaDeTeclas.length){
     const efeito=listaDeTeclas[contador].classList[1];
     const idAudio= '#som_' +efeito;
-    listaDeTeclas[contador].onclick=fuction(){
+    listaDeTeclas[contador].onclick=function(){
         tocaSom(idAudio);
     }
     contador= contador+1;
